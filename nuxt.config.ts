ebaf7ca7dc25bca194bbfe7a2eaf8eebca08ed70
noxa38 <noxa38@gmail.com>
2026-05-20 08:47:53 +0200

    (feat)Changement de port pour la connexion a l'api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  devServer: {
+    port: 3001
+  },
   runtimeConfig: {
     public: {
-      apiBaseUrl: 'http://localhost:3001'
+      apiBaseUrl: 'http://localhost:3000'
     }
   }
 })
